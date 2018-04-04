93ad6ba11f6356002cfd4d02e4b009347803fc73
Roger <roger@www.edt.org>
2018-04-04 18:25:10 +0200

    kserver

---
+++ khost/install.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+dnf -y update vi
+dnf -y install vim nmap procps iproute iputils krb5-libs krb5-workstation util-linux-user finger pam_krb5
---
+++ khost/startup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+useradd anna
+useradd pere
+useradd pau 
+useradd marta
+useradd julia
+useradd jordi
+
+rm -rf /var/run/nologin 
