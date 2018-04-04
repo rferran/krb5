29dfcd4db74a3d762fe9f072051a40babbd57538
Roger <roger@www.edt.org>
2018-04-04 19:07:44 +0200

    rm install

--- kserver/install.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-dnf -y update vi
-dnf -y install nmap vim iputils tree iproute mlocate procps krb5-server krb5-libs krb5-workstation man-db
--- sshserver/install.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-dnf -y update vi
-dnf -y install vim nmap procps krb5-libs krb5-workstation openssh openssh-server openssh-clients passwd
